ef554c9457f4bee8247ebb0aff933d2a7989c328
minhazk <minhazulkarim26@gmail.com>
2023-06-09 17:50:57 +0100

    remove basket items

--- src/app/basket/page.tsx
+++ src/app/basket/page.tsx
@@ -2,63 +2,67 @@
 
 import { api } from '@/util/trpc';
 import { useSession } from 'next-auth/react';
-import Image from 'next/image';
 import NavigationHistory from '@/components/ui/NavigationHistory';
 import currencyFormatter from '@/util/currencyFormat';
 import CustomButton from '@/components/ui/CustomButton';
+import BasketItem, { removeItemProps } from '@/components/basket/BasketItem';
+import { showToast } from '@/util/toastNotification';
+import { useEffect, useState } from 'react';
+import { Loader2 } from 'lucide-react';
 
 export default function Page() {
     const { data: session } = useSession();
-    const { data: items } = api.items.getUserItems.useQuery(session?.user?.id ?? null);
-    if (session?.user?.id == null) return <div>not logged in</div>;
+    const { data: items, refetch } = api.items.getUserItems.useQuery(session?.user?.id ?? null);
+
+    const removeItem = api.items.removeItemFromBasket.useMutation({
+        onSuccess() {
+            showToast('Item removed from basket');
+        },
+        onError() {
+            showToast('There was an error removing your item.');
+        },
+    });
+
+    const handleRemoveItem = async ({ colourId, sizeId, itemId, quantity }: removeItemProps) => {
+        if (session?.user?.id == null) return;
+        await removeItem.mutateAsync({
+            userId: session.user.id,
+            colourId,
+            sizeId,
+            itemId,
+            quantity,
+        });
+        refetch();
+    };
 
     return (
         <>
             <NavigationHistory routes={['Basket']} />
             <div className='flex flex-col gap-5 md:flex-row md:gap-16'>
-                <div className='my-6 flex flex-col gap-3 md:w-[60%]'>
-                    {items?.map(({ item, colour, size, quantity }, i) => (
-                        <div key={i} className='flex items-center gap-2 sm:gap-5'>
-                            <div className='relative aspect-square min-w-[92px] overflow-hidden rounded-md sm:min-w-[96px]'>
-                                <Image src={item.mainImage!.url} fill className='object-cover' alt={item.name} />
-                            </div>
-                            <div className='flex flex-grow flex-col gap-1'>
-                                <p
-                                    className='overflow-hidden text-ellipsis text-sm font-semibold text-slate-600 first-letter:uppercase sm:text-base'
-                                    style={{ display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical' }}
-                                >
-                                    {item.name}
-                                </p>
-                                <div className='flex flex-col text-xs sm:text-sm'>
-                                    <p className='text-slate-500'>
-                                        Colour:<span className='mx-2 font-semibold'>{colour.name}</span>
-                                    </p>
-                                    <p className='text-slate-500'>
-                                        Size:<span className='mx-2 font-semibold'> {size.name}</span>
-                                    </p>
-                                </div>
-                            </div>
-                            <div className='flex min-w-[95px] flex-col items-center gap-2'>
-                                <p className='text-sm font-medium text-slate-600 sm:text-sm'>
-                                    {item.oldPrice != null && <span className='mr-2 text-xs text-slate-400 line-through'>{currencyFormatter(item.oldPrice)}</span>}
-                                    {currencyFormatter(item.price)}
-                                </p>
-                                <p className='font-semibold text-slate-600'>
-                                    <span className='mr-1 text-xs font-light text-slate-500'>x{quantity}</span>
-                                    {currencyFormatter(item.price * quantity)}
-                                </p>
-                            </div>
+                <div className='relative mb-6 flex flex-col gap-3 md:w-[60%]'>
+                    {removeItem.isLoading && (
+                        <div className='absolute inset-0 z-40 flex items-center justify-center gap-3 bg-slate-300/40 text-slate-400'>
+                            <Loader2 size={18} className='animate-spin' />
+                            Loading
                         </div>
-                    ))}
+                    )}
+                    <h1 className='mb-3 text-xl font-semibold text-slate-600'>Your items</h1>
+                    {!items || items.length === 0 ? (
+                        <p className='py-8 text-center text-sm text-slate-400'>There are no items in your basket</p>
+                    ) : (
+                        items?.map((basketItem: any, i: any) => <BasketItem {...basketItem} removeItem={handleRemoveItem} key={i} loading={removeItem.isLoading} />)
+                    )}
                 </div>
 
                 <div className='md:w-[40%]'>
                     <h1 className='mb-4 text-xl font-semibold text-slate-600'>Summary</h1>
                     <div className='grid grid-cols-2 gap-3 text-slate-700'>
                         <p className='text-sm font-medium sm:text-base'>Subtotal</p>
-                        <p className='justify-self-end text-sm sm:text-base'>{currencyFormatter(items?.reduce((prev, curr) => prev + curr.item.price * curr.quantity, 0) ?? 0)}</p>
+                        <p className='justify-self-end text-sm sm:text-base'>{currencyFormatter(items?.reduce((prev: any, curr: any) => prev + curr.item.price * curr.quantity, 0) ?? 0)}</p>
                         <p className='text-sm font-medium sm:text-base'>Total</p>
-                        <p className='justify-self-end text-sm sm:text-base'>{currencyFormatter(items?.reduce((prev, curr) => prev + curr.item.price * curr.quantity, 0) ?? 0)}</p>
+                        <p className='justify-self-end text-sm font-semibold sm:text-base'>
+                            {currencyFormatter(items?.reduce((prev: any, curr: any) => prev + curr.item.price * curr.quantity, 0) ?? 0)}
+                        </p>
                         <div className='col-span-2 my-3 h-px bg-gray-200'></div>
                         <div className='col-span-2'>
                             <CustomButton>Stripe</CustomButton>
---
+++ src/components/basket/BasketItem.tsx
@@ -0,0 +1,77 @@
+import currencyFormatter from '@/util/currencyFormat';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export type BasketItemProps = {
+    item: {
+        mainImage: {
+            url: string;
+        } | null;
+        name: string;
+        oldPrice: number | null;
+        price: number;
+        id: number;
+    };
+    colour: { id: number; name: string };
+    size: { id: number; name: string };
+    quantity: number;
+    removeItem: (data: removeItemProps) => void;
+    loading: boolean;
+};
+
+export type removeItemProps = {
+    colourId: number;
+    sizeId: number;
+    itemId: number;
+    quantity: number;
+};
+
+export default function BasketItem({ item, colour, size, quantity, removeItem, loading }: BasketItemProps) {
+    const handleRemoveItem = (e: React.MouseEvent<HTMLElement>) => {
+        e.stopPropagation();
+        void removeItem({
+            colourId: colour.id,
+            sizeId: size.id,
+            itemId: item.id,
+            quantity,
+        });
+    };
+
+    return (
+        <div className='relative'>
+            <button onClick={handleRemoveItem} disabled={loading} className='peer flex w-full cursor-pointer items-center gap-2 text-left hover:bg-red-50 sm:gap-5'>
+                <div className='relative aspect-square min-w-[92px] overflow-hidden rounded-md sm:min-w-[96px]'>
+                    <Image src={item.mainImage!.url} fill className='object-cover' alt={item.name} />
+                </div>
+                <div className='flex flex-grow flex-col gap-1'>
+                    <Link
+                        href={`/shop/${item.id}`}
+                        className='overflow-hidden text-ellipsis text-sm font-semibold text-slate-600 first-letter:uppercase sm:text-base'
+                        style={{ display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical' }}
+                    >
+                        {item.name}
+                    </Link>
+                    <div className='flex flex-col text-xs sm:text-sm'>
+                        <p className='text-slate-500'>
+                            Colour:<span className='mx-2 font-semibold'>{colour.name}</span>
+                        </p>
+                        <p className='text-slate-500'>
+                            Size:<span className='mx-2 font-semibold'> {size.name}</span>
+                        </p>
+                    </div>
+                </div>
+                <div className='flex min-w-[95px] flex-col items-center gap-2'>
+                    <p className='text-sm font-medium text-slate-600 sm:text-sm'>
+                        {item.oldPrice != null && <span className='mr-2 text-xs text-slate-400 line-through'>{currencyFormatter(item.oldPrice)}</span>}
+                        {currencyFormatter(item.price)}
+                    </p>
+                    <p className='font-semibold text-slate-600'>
+                        <span className='mr-1 text-xs font-light text-slate-500'>x{quantity}</span>
+                        {currencyFormatter(item.price * quantity)}
+                    </p>
+                </div>
+            </button>
+            <div className='pointer-events-none absolute bottom-4 left-1/2 hidden -translate-x-1/2 text-center text-xs font-semibold text-red-500 peer-hover:block'>Remove</div>
+        </div>
+    );
+}
