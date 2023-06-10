ed815a962d76dbc89847782d744d0c0b705c9c06
minhazk <minhazulkarim26@gmail.com>
2023-06-10 23:54:12 +0100

    removed deprecated config export

--- src/app/api/stripe-webhook/route.ts
+++ src/app/api/stripe-webhook/route.ts
@@ -56,9 +56,3 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json({ received: true });
 }
-
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-};
