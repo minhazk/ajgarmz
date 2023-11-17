538d1b1180952cf44544181ae2f254434d160355
minhazk <minhazulkarim26@gmail.com>
2023-11-17 01:43:13 +0000

    query limit change

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -24,7 +24,7 @@ export default function Page({ searchParams: { category } }: PageProps) {
         isLoading,
         refetch,
     } = api.items.getAll.useInfiniteQuery(
-        { filters: appliedFilters, searchParam: category?.toLowerCase(), limit: 100 },
+        { filters: appliedFilters, searchParam: category?.toLowerCase(), limit: 50 },
         {
             getNextPageParam: lastPage => lastPage.nextCursor,
         }
